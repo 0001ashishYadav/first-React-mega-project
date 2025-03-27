7e7dbb29c04797bb663b1f5b81e74b5f5696bcd9
000ashishYadav <000ashishyadav2003@gmail.com>
2025-03-27 11:24:23 +0530

    initialize my first mega project using of react + vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-800 text-3xl font-bold">
+        my first react mega project
+      </h1>
+    </>
+  );
+}
+
+export default App;
